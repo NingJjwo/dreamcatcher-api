e72657c1a9d6a29a1b0b5ae8bc34a82de00135fb
Sakura <ixxjuandavidgxxi@gmail.com>
2025-07-22 19:46:18 -0500

    Fixed Getter & Setter issue

--- src/main/java/com/dreamcatcher/dreamcatcherapi/model/Album.java
+++ src/main/java/com/dreamcatcher/dreamcatcherapi/model/Album.java
@@ -1,11 +1,14 @@
 package com.dreamcatcher.dreamcatcherapi.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-
+@Getter
+@Setter
 @Data
 @Document(collection = "albums")
 public class Album {
