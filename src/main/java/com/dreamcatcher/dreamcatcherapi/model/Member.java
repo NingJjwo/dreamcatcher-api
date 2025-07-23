106ff8766b6645c699917346f5ec526646e10429
Sakura <ixxjuandavidgxxi@gmail.com>
2025-07-23 17:57:43 -0500

    New attribute for image resources

--- src/main/java/com/dreamcatcher/dreamcatcherapi/model/Member.java
+++ src/main/java/com/dreamcatcher/dreamcatcherapi/model/Member.java
@@ -1,10 +1,14 @@
 package com.dreamcatcher.dreamcatcherapi.model;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @NoArgsConstructor
+@Setter
+@Getter
 @Document(collection = "members")
 public class Member {
 
@@ -18,8 +22,10 @@ public class Member {
     private String nationality;
     private String cityBorn;
     private String nightmare;
+    private String image;
 
-    public Member(Long id, String realName, String stageName, String koreanName, String role, String dateBorn, String nationality, String cityBorn, String nightmare) {
+    public Member(Long id, String realName, String stageName, String koreanName, String role, String dateBorn, String nationality, String cityBorn, String nightmare,
+        String image) {
         this.id = id;
         this.realName = realName;
         this.stageName = stageName;
@@ -29,69 +35,6 @@ public class Member {
         this.nationality = nationality;
         this.cityBorn = cityBorn;
         this.nightmare = nightmare;
+        this.image = image;
     }
-
-    public void setId(long id){
-        this.id = id;
-    }
-    public long getId(){
-        return id;
-    }
-
-    public void setRealName(String realName){
-        this.realName = realName;
-    }
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setStageName(String stageName){
-        this.stageName = stageName;
-    }
-    public String getStageName() {
-        return stageName;
-    }
-
-    public void setKoreanName(String koreanName){
-        this.koreanName = koreanName;
-    }
-    public String getKoreanName() {
-        return koreanName;
-    }
-
-    public void setRole(String role){
-        this.role = role;
-    }
-    public String getRole() {
-        return role;
-    }
-
-    public void setDateBorn(String dateBorn){
-        this.dateBorn = dateBorn;
-    }
-    public String getDateBorn() {
-        return dateBorn;
-    }
-
-    public void setNationality(String nationality){
-        this.nationality = nationality;
-    }
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setCityBorn(String cityBorn){
-        this.cityBorn = cityBorn;
-    }
-    public String getCityBorn() {
-        return cityBorn;
-    }
-
-    public void setNightmare(String nightmare){
-        this.nightmare = nightmare;
-    }
-    public String getNightmare(){
-        return nightmare;
-    }
-
 }
