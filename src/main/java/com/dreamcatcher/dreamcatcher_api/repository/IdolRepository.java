5bb10cc0e748611573c42489602a466fef868d6c
NingJjwo <ixxjuandavidgxxi@gmail.com>
2026-09-10 13:41:15 -0500

    fix: drop redundant repository annotation

--- src/main/java/com/dreamcatcher/dreamcatcher_api/repository/IdolRepository.java
+++ src/main/java/com/dreamcatcher/dreamcatcher_api/repository/IdolRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.dreamcatcher.dreamcatcher_api.model.Idol;
 
-@Repository
 public interface IdolRepository extends JpaRepository<Idol, Long> {
 
     @EntityGraph(attributePaths = { "group", "positions" })
