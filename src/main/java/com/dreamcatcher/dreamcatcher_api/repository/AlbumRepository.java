c11df60fc723949492af5c837c966019a3a4941a
NingJjwo <ixxjuandavidgxxi@gmail.com>
2026-09-10 13:41:14 -0500

    fix: drop redundant repository annotation

--- src/main/java/com/dreamcatcher/dreamcatcher_api/repository/AlbumRepository.java
+++ src/main/java/com/dreamcatcher/dreamcatcher_api/repository/AlbumRepository.java
@@ -3,11 +3,9 @@ package com.dreamcatcher.dreamcatcher_api.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.dreamcatcher.dreamcatcher_api.model.Album;
 
-@Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
     List<Album> findByGroup_Id(Long groupId);
 }
