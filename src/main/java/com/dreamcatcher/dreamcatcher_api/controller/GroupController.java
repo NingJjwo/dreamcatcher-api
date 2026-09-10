a07c7b8c1b590db6fc165f585c3fcddefc1e0ebe
NingJjwo <ixxjuandavidgxxi@gmail.com>
2026-09-10 13:40:18 -0500

    feat: add get group by id endpoint

--- src/main/java/com/dreamcatcher/dreamcatcher_api/controller/GroupController.java
+++ src/main/java/com/dreamcatcher/dreamcatcher_api/controller/GroupController.java
@@ -2,11 +2,12 @@ package com.dreamcatcher.dreamcatcher_api.controller;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dreamcatcher.dreamcatcher_api.dto.GroupDetailResponseDto;
 import com.dreamcatcher.dreamcatcher_api.dto.GroupResponseDto;
 import com.dreamcatcher.dreamcatcher_api.service.GroupService;
 
@@ -20,7 +21,12 @@ public class GroupController {
     private final GroupService groupService;
 
     @GetMapping
-    public ResponseEntity<List<GroupResponseDto>> getAllGroups() {
-        return ResponseEntity.ok(groupService.findAll());
+    public List<GroupResponseDto> getAllGroups() {
+        return groupService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public GroupDetailResponseDto getGroupById(@PathVariable Long id) {
+        return groupService.findById(id);
     }
 }
