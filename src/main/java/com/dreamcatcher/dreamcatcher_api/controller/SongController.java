a088b567cd782be0fbd2c92bdf104d6eb459525b
NingJjwo <ixxjuandavidgxxi@gmail.com>
2026-09-10 13:41:08 -0500

    fix: return plain types from get endpoints

--- src/main/java/com/dreamcatcher/dreamcatcher_api/controller/SongController.java
+++ src/main/java/com/dreamcatcher/dreamcatcher_api/controller/SongController.java
@@ -2,11 +2,10 @@ package com.dreamcatcher.dreamcatcher_api.controller;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dreamcatcher.dreamcatcher_api.dto.SongResponseDto;
 import com.dreamcatcher.dreamcatcher_api.service.SongService;
@@ -21,17 +20,17 @@ public class SongController {
     private final SongService songService;
 
     @GetMapping
-    public ResponseEntity<List<SongResponseDto>> getAllSongs() {
-        return ResponseEntity.ok(songService.findAll());
+    public List<SongResponseDto> getAllSongs() {
+        return songService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SongResponseDto> getSongById(@PathVariable Long id) {
-        return ResponseEntity.ok(songService.findById(id));
+    public SongResponseDto getSongById(@PathVariable Long id) {
+        return songService.findById(id);
     }
 
     @GetMapping("/album/{albumId}")
-    public ResponseEntity<List<SongResponseDto>> getSongsByAlbum(@PathVariable Long albumId) {
-        return ResponseEntity.ok(songService.findAllByAlbumId(albumId));
+    public List<SongResponseDto> getSongsByAlbum(@PathVariable Long albumId) {
+        return songService.findAllByAlbumId(albumId);
     }
 }
