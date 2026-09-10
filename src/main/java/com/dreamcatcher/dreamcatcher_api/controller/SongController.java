7f0a2dd952d79e33a11c3d666fddc7981ba690b1
NingJjwo <ixxjuandavidgxxi@gmail.com>
2026-09-10 14:24:04 -0500

    feat: document song endpoints with openapi annotations

--- src/main/java/com/dreamcatcher/dreamcatcher_api/controller/SongController.java
+++ src/main/java/com/dreamcatcher/dreamcatcher_api/controller/SongController.java
@@ -10,21 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dreamcatcher.dreamcatcher_api.dto.SongResponseDto;
 import com.dreamcatcher.dreamcatcher_api.service.SongService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/songs")
 @RequiredArgsConstructor
+@Tag(name = "Songs", description = "K-pop songs")
 public class SongController {
 
     private final SongService songService;
 
     @GetMapping
+    @Operation(summary = "List all songs")
     public List<SongResponseDto> getAllSongs() {
         return songService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a song by id")
     public SongResponseDto getSongById(@PathVariable Long id) {
         return songService.findById(id);
     }
