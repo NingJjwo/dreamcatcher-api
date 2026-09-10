09dabba3107812a2f9a03abe7a31de37150b70a0
NingJjwo <ixxjuandavidgxxi@gmail.com>
2026-09-10 13:41:08 -0500

    fix: return plain types from get endpoints

--- src/main/java/com/dreamcatcher/dreamcatcher_api/controller/AlbumController.java
+++ src/main/java/com/dreamcatcher/dreamcatcher_api/controller/AlbumController.java
@@ -2,11 +2,10 @@ package com.dreamcatcher.dreamcatcher_api.controller;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.dreamcatcher.dreamcatcher_api.dto.AlbumResponseDto;
 import com.dreamcatcher.dreamcatcher_api.service.AlbumService;
@@ -21,17 +20,17 @@ public class AlbumController {
     private final AlbumService albumService;
 
     @GetMapping
-    public ResponseEntity<List<AlbumResponseDto>> getAllAlbums() {
-        return ResponseEntity.ok(albumService.findAll());
+    public List<AlbumResponseDto> getAllAlbums() {
+        return albumService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<AlbumResponseDto> getAlbumById(@PathVariable Long id) {
-        return ResponseEntity.ok(albumService.findById(id));
+    public AlbumResponseDto getAlbumById(@PathVariable Long id) {
+        return albumService.findById(id);
     }
 
     @GetMapping("/group/{groupId}")
-    public ResponseEntity<List<AlbumResponseDto>> getAlbumsByGroup(@PathVariable Long groupId) {
-        return ResponseEntity.ok(albumService.findAllByGroupId(groupId));
+    public List<AlbumResponseDto> getAlbumsByGroup(@PathVariable Long groupId) {
+        return albumService.findAllByGroupId(groupId);
     }
 }
